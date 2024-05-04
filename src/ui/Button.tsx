12c083d6510ba5bece5ef13c563d443507df15f4
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-05-04 17:26:08 +0300

    Tailwind: New Order

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -9,13 +9,13 @@ interface ButtonProps {
 
 const Button = ({ children, disabled, to, type = "primary" }: ButtonProps) => {
     const base =
-        "bg-yellow-400 uppercase font-semibold text-stone-800  inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+        "bg-yellow-400 uppercase font-semibold text-sm text-stone-800  inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
     const styles = {
         primary: base + " py-3 px-4 md:px-6 md:py-4",
         small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
         secondary:
-            "uppercase font-semibold text-stone-400 border-2 border-stone-300 inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:text-stone-800 focus:ring-offset-2 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5",
+            "uppercase font-semibold text-sm text-stone-400 border-2 border-stone-300 inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:text-stone-800 focus:ring-offset-2 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5",
     };
 
     if (to) {
