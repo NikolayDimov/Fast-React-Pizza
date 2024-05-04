b707926d8b89da5279d108b851d853264cacc39e
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-05-04 17:50:49 +0300

    Tailwind: OrderItem

--- src/features/order/Order.tsx
+++ src/features/order/Order.tsx
@@ -3,6 +3,7 @@
 import { useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import { calcMinutesLeft, formatCurrency, formatDate } from "../../utils/helpers";
+import OrderItem from "./OrderItem";
 
 export interface PizzaOrder {
     pizzaId: number;
@@ -67,38 +68,42 @@ function Order() {
         return <div>Loading...</div>;
     }
     // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
-    const {
-        // id,
-        status,
-        priority,
-        priorityPrice,
-        orderPrice,
-        estimatedDelivery,
-        // cart
-    } = order;
+    const { id, status, priority, priorityPrice, orderPrice, estimatedDelivery, cart } = order;
 
     const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
     return (
-        <div>
-            <div>
-                <h2>Status</h2>
+        <div className="px-4 py-6 space-y-8">
+            <div className="flex gap-2 items-center justify-between flex-wrap">
+                <h2 className="text-xl font-semibold">Order #{id} status</h2>
 
-                <div>
-                    {priority && <span>Priority</span>}
-                    <span>{status} order</span>
+                <div className="space-x-2">
+                    {priority && (
+                        <span className="bg-red-500 rounded-full py-1 px-3 text-sm uppercase font-semibold text-red-50">Priority</span>
+                    )}
+                    <span className="bg-green-500 rounded-full py-1 px-3 text-sm uppercase font-semibold text-green-50">
+                        {status} order
+                    </span>
                 </div>
             </div>
 
-            <div>
-                <p>{deliveryIn >= 0 ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃` : "Order should have arrived"}</p>
-                <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+            <div className="flex gap-2 bg-stone-200 py-5 px-6 items-center justify-between flex-wrap">
+                <p className="font-medium">
+                    {deliveryIn >= 0 ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃` : "Order should have arrived"}
+                </p>
+                <p className="text-xs text-stone-500">(Estimated delivery: {formatDate(estimatedDelivery)})</p>
             </div>
 
-            <div>
-                <p>Price pizza: {formatCurrency(orderPrice)}</p>
-                {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-                {priorityPrice && <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>}
+            <ul className="divide-y divide-stone-200 border-b border-t">
+                {cart.map((item) => (
+                    <OrderItem item={item} key={item.pizzaId} />
+                ))}
+            </ul>
+
+            <div className="space-y-2 bg-stone-200 py-5 px-6">
+                <p className="text-sm font-medium text-stone-600">Price pizza: {formatCurrency(orderPrice)}</p>
+                {priority && <p className="text-sm font-medium text-stone-600">Price priority: {formatCurrency(priorityPrice)}</p>}
+                {priorityPrice && <p className="font-bold">To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>}
             </div>
         </div>
     );
