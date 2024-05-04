26b9b13e0a6879019e49af1491892ae7fa778b10
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-05-04 22:46:50 +0300

    Add Redux and username with Redux

--- src/features/user/Username.tsx
+++ src/features/user/Username.tsx
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
+
+type RootState = {
+    user: {
+        username: string;
+    };
+};
+
 const Username: React.FC = () => {
-    return <div className="hidden text-sm font-semibold md:block ">Niki</div>;
+    const username = useSelector((state: RootState) => state.user.username);
+
+    if (!username) return null;
+
+    return <div className="hidden text-sm font-semibold md:block">{username}</div>;
 };
 
 export default Username;
